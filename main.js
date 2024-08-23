d4ad36e392302bf40462d72ba787401b76968555
anagrzibovski <166126799+anagrzibovski@users.noreply.github.com>
2024-08-23 10:11:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let nomeUsuario = ""; 
+let element = document.querySelector("#nome=usuario");
+
+while(nomeUsuario ==""){
+     nomeUsuario == prompt("Qual seu nome?");
+
+}
+
+ if (nomeUsuario == null){
+  element,textcontent = "seja bem vinda";
+ } else {
+    element.textcontent == nomeUsuario;
+
+  }
